78976a2dc9180ec66bafb8eb85413583d4f13e60
D S Lituiev <d.lituiev@gmail.com>
2015-02-17 23:46:05 +0100

    cleaned install.sh; added a batch script

--- install.sh
+++ install.sh
@@ -2,11 +2,17 @@
 
 cp libs/sqlite3pp_src_CMakeLists.txt  libs/sqlite3pp/src/CMakeLists.txt
 
-echo "Downloading SQLite3 base..."
-wget  -N -q -A.zip "http://www.sqlite.org/2015/sqlite-amalgamation-3080802.zip" -P "./libs" || exit 1;
-cp "libs/sqlite3pp_src_sqlite_CMakeLists.txt" "libs/sqlite3pp/src/sqlite-amalgamation-3080802/CMakeLists.txt"
+FLAG_SQLITE_INCLUDED=1;
 
-unzip "libs/sqlite-amalgamation-3080802.zip" -d "libs/sqlite3pp/src/" || exit 1;
+if [ ! $FLAG_SQLITE_INCLUDED  ]
+then 
+    echo "Downloading SQLite3 base..."
+    wget  -N -q -A.zip "http://www.sqlite.org/2015/sqlite-amalgamation-3080802.zip" -P "./libs" || exit 1;
+    cp "libs/sqlite3pp_src_sqlite_CMakeLists.txt" "libs/sqlite3pp/src/sqlite-amalgamation-3080802/CMakeLists.txt"
+    unzip "libs/sqlite-amalgamation-3080802.zip" -d "libs/sqlite3pp/src/" || exit 1;
+else
+    echo "====================================================="
+fi
 
 mkdir build
 cd build || exit 1;
