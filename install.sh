0cd30c5ef1a1b4558c37512c8cd0b4d68a9bf047
D S Lituiev <d.lituiev@gmail.com>
2015-02-17 11:58:26 +0100

    installation fix

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 cp libs/sqlite3pp_src_CMakeLists.txt  libs/sqlite3pp/src/CMakeLists.txt
 
+unzip "libs/sqlite-amalgamation-3080802.zip" -d "libs/sqlite3pp/src/" || exit 1;
+
 mkdir build
 cd build || exit 1;
 rm ./* -rf
 
-cmake ..
+cmake .. || exit 1;
 
-make
+make || exit 1;
 
-cd ../bin
+cd ../bin || ( echo "hmm. something went wrong... \n No 'bin' directory has been produced."; exit 1;)
 
 ls -l
 
